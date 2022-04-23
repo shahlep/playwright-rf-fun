e849a9000210f93b9110ab7d0098bae6e0b51054
Shah rahman <toshah.rahman@gmail.com>
2022-04-23 17:37:21 +0300

    playwright rf fun 4

--- Library/PyLib/PlaywrightCore.py
+++ Library/PyLib/PlaywrightCore.py
@@ -11,24 +11,28 @@ class PlaywrightCore:
     def launch_browser(browser_name):
         print("Browser : " + browser_name)
         PlaywrightCore.pwSync = sync_playwright().start()
-        if browser_name.lower() == "Chromium":
+        if browser_name.lower() == "chromium":
             PlaywrightCore.browser = PlaywrightCore.pwSync.chromium.launch(headless=False)
+        elif browser_name.lower() == "firefox":
+            PlaywrightCore.browser = PlaywrightCore.pwSync.firefox.launch(headless=False)
+        else:
+            PlaywrightCore.browser = PlaywrightCore.pwSync.webkit.launch(headless=False)
+        PlaywrightCore.context = PlaywrightCore.browser.new_context()
 
     @staticmethod
     def close_browser():
+        PlaywrightCore.context.close()
         PlaywrightCore.browser.close()
         PlaywrightCore.pwSync.stop()
 
     @staticmethod
     def open_application():
-        PlaywrightCore.context = PlaywrightCore.browser.new_context()
         PlaywrightCore.page = PlaywrightCore.context.new_page()
         PlaywrightCore.page.goto('https://demo.playwright.dev/todomvc/#/')
 
     @staticmethod
     def close_application():
         PlaywrightCore.page.close()
-        PlaywrightCore.context.close()
 
     @staticmethod
     def get_page_object():
